4cb953566414b43c27606b0268f8a3a1e480be13
Aseem Regmi <aseem.regmi22@gmail.com>
2019-07-30 07:34:54 +0545

    Updated Run Scripts

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Middleware
@@ -6,44 +6,44 @@ app.use(express.json());
 var http = require('http');
 var cors = require('cors');
 var mysql = require('mysql');
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 
 pool = mysql.createPool({
-	connectionLimit: 100,
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "department_store",
-	supportBigNumbers: true,
-	bigNumberStrings: true
+  connectionLimit: 100,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'department_store',
+  supportBigNumbers: true,
+  bigNumberStrings: true
 });
 
-app.get("/", (req, res) => {
-	res.send("Welcome to API Server of Department Store Management System");
+app.get('/', (req, res) => {
+  res.send('Welcome to API Server of Department Store Management System');
 });
 
 const employeeRoute = require('./routes/api/employee');
-app.use("/api/", employeeRoute);
+app.use('/api/', employeeRoute);
 
 const customerRoute = require('./routes/api/customer');
-app.use("/api/", customerRoute);
+app.use('/api/', customerRoute);
 
 const supplierRoute = require('./routes/api/supplier');
-app.use("/api/", supplierRoute);
+app.use('/api/', supplierRoute);
 
 const productsRoute = require('./routes/api/products');
-app.use("/api/", productsRoute);
+app.use('/api/', productsRoute);
 
 const ordersRoute = require('./routes/api/orders');
-app.use("/api/", ordersRoute);
+app.use('/api/', ordersRoute);
 
 const reviewsRoute = require('./routes/api/reviews');
-app.use("/api/", reviewsRoute);
+app.use('/api/', reviewsRoute);
 
 const billingsRoute = require('./routes/api/billings');
-app.use("/api/", billingsRoute);
+app.use('/api/', billingsRoute);
 
 app.listen(port, () => {
-	console.log(`Server is listening in port ${port}`);
+  console.log(`Server is listening in port ${port}`);
 });
